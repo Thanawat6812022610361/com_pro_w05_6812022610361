75ee8e4f932e4a2c73532d74cd2e317be360c6b6
Thanawat6812022610361 <noonmobile10947@gmail.com>
2025-07-21 15:23:42 +0700

    work 1

---
+++ coding_w5_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    char name = 'T';
+    int age = 18;
+    float weight = 73.55;
+
+    printf("Student %c is %d years old\n",name ,age);
+    printf("His weight is %.1f kg.",weight);
+    return 0;
+}
