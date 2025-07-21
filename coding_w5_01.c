c9accc5d320f9d857da8fec667744d12a0419a51
Thanawat6812022610361 <noonmobile10947@gmail.com>
2025-07-21 16:13:24 +0700

    Work 1-4

--- coding_w5_01.c
+++ coding_w5_01.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    char name = 'T';
-    int age = 18;
-    float weight = 73.55;
+    char name = 'T'; //ป้อนตัวแปรตัวอักษร
+    int age = 18; //ป้อนจำนวนเต็ม
+    float weight = 73.55; //ป้อนน้ำหนักเป็น float เพราะเป็นทศนิยม
 
-    printf("Student %c is %d years old\n",name ,age);
+    printf("Student %c is %d years old\n",name ,age); //ปริ้นตามที่เรากำหนดไว้ด้านบน
     printf("His weight is %.1f kg.",weight);
     return 0;
 }
